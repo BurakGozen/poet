816fc504c8ea2dde8484f472ea519ffa66661a16
BurakGozen <info@newcode.be>
2024-03-16 13:08:48 +0100

    fix: type error

--- src/app/(app)/poems/[id]/page.tsx
+++ src/app/(app)/poems/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { PageProps } from "@/lib/types/page";
 import configPromise from "@payload-config";
-import { getPayload } from "@payloadcms/next/utilities/getPayload";
 import { notFound } from "next/navigation";
+import { getPayload } from "payload";
 
 export default async function PoemPage({ params }: PageProps) {
   const id = params.id as string;
@@ -17,7 +17,7 @@ export default async function PoemPage({ params }: PageProps) {
   return (
     <div className="flex min-h-[100dvh] items-center justify-center px-5 py-20">
       <article className="max-w-full">
-        {title && <h1 className="text-md -ml-4 mb-2 font-serif font-bold">{title}</h1>}
+        {!!title && <h1 className="text-md -ml-4 mb-2 font-serif font-bold">{title}</h1>}
         <p
           className="-ml-4 shrink-0 whitespace-pre text-wrap border-l pl-4 *:min-h-3.5"
           dangerouslySetInnerHTML={{ __html: body_html || "" }}
